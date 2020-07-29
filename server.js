3c4c4f3e6b72504123bd1b8f8a508e0b550eded1
Daniel Beckley <tomiwabeckley@gmail.com>
2020-07-29 22:11:31 +0100

    updated

--- server.js
+++ server.js
@@ -25,60 +25,13 @@ app.use(express.json());
 app.use(cors())
 
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-
-    ]
-}
 
 app.get('/', (req, res) => res.send('it is working'))
-
-
-
-
 app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) } )
-
 app.post('/signin', (req, res) => { handleSignIn(req, res, db, bcrypt) } )
-
-
 app.get('/profile/:id', (req, res) => { handleProfileUpdate(req, res, db) } )
-
 app.put('/image', (req, res) => { handleScoreIncrement(req, res, db) } )
-
 app.post('/imageurl', (req, res) => { handleApiCall(req, res) } )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(process.env.PORT || 3000, ()=> {
     console.log(`app is running on port ${process.env.PORT}`)
