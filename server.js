333e707ecb27ef6f045dc2b5b304779e9e2576c1
Daniel Beckley <tomiwabeckley@gmail.com>
2020-07-30 02:22:49 +0100

    redirect

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
+if(process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https') {
+            res.redirect(`https://${req.header('host')}${req.url}`)
+        } else {
+            next()
+        }
+    })
+}
 
 
 app.get('/', (req, res) => res.send('it is working'))
